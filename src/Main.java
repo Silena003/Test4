529472a654587e571befe3ba6e0933953cd08994
Elena <silena003@gmail.com>
2021-11-22 10:12:00 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int accountAmount = 100;
-        int replenishmentAmount1 = 500;
         int replenishmentAmount2 = 1500;
 
         boolean promotionalBonuses1 = false;
 
         int percent1 = promotionalBonuses1 ? 0 : 1;
-        int accountTotal1 = accountAmount + replenishmentAmount1;
-        int bonuses1 = percent1 * accountTotal1 / 100;
 
         int accountTotal2 = accountAmount + replenishmentAmount2;
         int bonuses2 = percent1 * accountTotal2 / 100;
-        if (replenishmentAmount1 <= 1000) {
-            bonuses1 = 0;
+        if (replenishmentAmount2 <= 1000) {
+            bonuses2 = 0;
         }
 
-        System.out.println("Сумма на счете1: " + accountTotal1);
-        System.out.println("Бонусы1: " + bonuses1);
         System.out.println("Сумма на счете2: " + accountTotal2);
         System.out.println("Бонусы2: " + bonuses2);
     }
